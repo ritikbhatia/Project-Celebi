a4df3cc97bcbd844b05852e21c5b088a4c8f3fe5
Aishik Nagar <aishiknagar@gmail.com>
2021-09-18 23:27:43 +0800

    Logo to src

--- frontend/src/containers/config.js
+++ frontend/src/containers/config.js
@@ -1,2 +1,3 @@
 export const GoogleMapsAPI = 'AIzaSyBv8IZVkta7CgAa3nU3pwhtAklZ14tqNPc';
-export const NewsAPI = '00c8d3561b9642528ce180bc37dac50e'
+// export const NewsAPI = '00c8d3561b9642528ce180bc37dac50e'
+export const NewsAPI = '4577c98d7c8a41f6a144ee2c492e95bc'
